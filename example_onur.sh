ac5b57012dc1abd828f6100c576a587de27b9c15
sergeicu <serge.vasylechko@gmail.com>
2023-05-05 14:05:47 -0400

    Update example_onur.sh

--- example_onur.sh
+++ example_onur.sh
@@ -1,3 +1,29 @@
+##########
+# Run analysis on single nifti file (using best method, as noted from empirical studies)
+##########
+
+# locate codedir (installed for onur) 
+codedir=/fileserver/fastscratch/serge/for_onur/multicomponent-T2-toolbox
+
+# activate python 
+source $codedir/venv/bin/activate 
+
+# provide name of nifti 
+f=<4D_nifti>
+
+# [optional] create mask file 
+python3 $codedir/create_seg.py $f
+
+# provide mask file 
+mask=<mask_file>
+
+# run 
+python3 $codedir/run_real_data_script.py --input $f --minTE 8.2 --nTE 18 --TR 1000 --path_to_folder $PWD/ \
+    --mask $mask --savefig no --savefig_slice 0 --FA_method spline --FA_smooth no --denoise TV \
+    --reg_method T2SPARC \
+    --reg_matrix I \
+    --myelin_T2_cutoff 30
+
 
 ##########
 # Setup environment 
@@ -19,31 +45,4 @@ source venv/bin/activate
 
 # install all python libraries
 pip install -r requirements.txt
-
-
-##########
-# Run analysis on single nifti file (using best method, as noted from empirical studies)
-##########
-
-# locate codedir
-codedir=<path_to_repository>
-
-# activate python 
-source $codedir/venv/bin/activate 
-
-# provide name of nifti 
-f=<4D_nifti>
-
-# [optional] create mask file 
-python3 $codedir/create_seg.py $f
-
-# provide mask file 
-mask=<mask_file>
-
-# run 
-python3 $codedir/run_real_data_script.py --input $f --minTE 8.2 --nTE 18 --TR 1000 --path_to_folder $PWD/ \
-    --mask $mask --savefig no --savefig_slice 0 --FA_method spline --FA_smooth no --denoise TV \
-    --reg_method T2SPARC \
-    --reg_matrix I \
-    --myelin_T2_cutoff 30
 
